e5f8ed0ea3e08ecce7d62b04681bf07f6f5e045f
Ramin Jalili <raminjalili2507@gmail.com>
2024-10-15 15:53:00 +0200

    table user peuplé

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -4,13 +4,39 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Enum\UserRole; // Assurez-vous d'importer l'énumération des rôles si nécessaire
 
 class UserFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
-    {
-        // $product = new Product();
-        // $manager->persist($product);
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher) {
+        $this->hasher = $hasher;
+    }
+
+    public function load(ObjectManager $manager): void {
+        $faker = Factory::create('fr_FR');
+
+        $noms = ["Luca", "Marina", "Mathis", "Laurent", "Neymar", "Ronaldo", "Kaka", "Jessica"];
+        
+        for ($i = 0; $i < count($noms); $i++) {
+            $user = new User();
+            $user->setNom($noms[$i]);
+            $user->setEmail($faker->unique()->email);
+            
+            $user->setPassword($this->hasher->hashPassword($user, 'Password123!'));
+
+            if ($i === 1 || $i === 3 || $i === 4) {
+                $user->setRole(UserRole::ADMIN);
+            } else {
+                $user->setRole(UserRole::USER);
+            }
+
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
